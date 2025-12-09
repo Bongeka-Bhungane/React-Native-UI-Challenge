b7f3e85e0f90c0b2933f59e3fa6744cb00e7d171
Bongeka-Bhungane <angelabhungane@gmsil.com>
2025-12-09 10:41:02 +0200

    made a component for the tab bar

--- Halodex/components/TabBar.tsx
+++ Halodex/components/TabBar.tsx
@@ -1,58 +1,71 @@
-import { StyleSheet, Text, View, Pressable } from 'react-native'
-import React, { useState } from 'react'
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import React, { useState } from "react";
 
-const tabs = ['Chat', 'Status', 'Calls']
+const tabs = ["Chat", "Status", "Calls"];
 
 const TabBar = () => {
-    const [selected, setSelected] = useState("Chat")
+  const [selected, setSelected] = useState("Chat");
   return (
     <View style={styles.container}>
       {tabs.map((t) => {
         const active = t === selected;
         return (
-            <Pressable
+          <Pressable
             key={t}
             onPress={() => setSelected(t)}
-            style={({ pressed}) => [
-                styles.tab, 
-                active && styles.activeTab,
-                pressed && { opacity: 0.8 }
+            style={({ pressed }) => [
+              styles.tab,
+              active && styles.activeTab,
+              pressed && { opacity: 0.8 },
             ]}
-            >
-                {active ? (
-                    <View style={styles.activeInner}>
-                        <Text style={styles.tabText}></Text>
-                    </View>
-                ): }
-            </Pressable>
-        )
+          >
+            {active ? (
+              <View style={[styles.activeInner]}>
+                <Text style={styles.activeText}>{t}</Text>
+              </View>
+            ) : (
+              <Text style={styles.tabText}>{t}</Text>
+            )}
+          </Pressable>
+        );
       })}
     </View>
-  )
-}
+  );
+};
 
-export default TabBar
+export default TabBar;
 
 const styles = StyleSheet.create({
-    container: {
-        marginVertical: 8,
-        flexDirection: 'row',
-        backgroundColor: '#FFF6F1',
-        padding: 6,
-        borderRadius: 12,
-        alignSelf: 'stretch',
-    },
-    tab: {
-        color: '#B15F3B',
-        fontWeight: '600'
-    },
-    activeTab: {
-
-    },
-    activeInner: {
-
-    },
-    tabText: {
-        
-    }
-})
+  container: {
+    marginVertical: 8,
+    flexDirection: "row",
+    backgroundColor: "#FEECE5",
+    padding: 6,
+    borderRadius: 12,
+    alignSelf: "stretch",
+  },
+  tab: {
+    flex: 1,
+    alignItems: "center",
+    paddingVertical: 8,
+  },
+  activeTab: {
+    backgroundColor: "#F3662C",
+    color: 'white',
+    borderRadius: 10,
+  },
+  activeInner: {
+    paddingHorizontal: 14,
+    paddingVertical: 6,
+    borderRadius: 10,
+    width: "100%",
+    alignItems: "center",
+  },
+  tabText: {
+    color: "#B15F3B",
+    fontWeight: "600",
+  },
+  activeText: {
+    color: "#FEECE5",
+  },
+});
